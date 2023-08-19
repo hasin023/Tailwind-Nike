bdb5e9b6e347ab6b39dfcfae6d914b3779285306
Hasin Mahtab <hasinmahtab.alvee@gmail.com>
2023-08-20 00:34:33 +0600

    Finished NAV Bar

---
+++ src/components/SignInButton.jsx
@@ -0,0 +1,14 @@
+const SignInButton = () => {
+  return (
+    <a href="#" className="relative px-12 py-2 overflow-hidden font-medium text-red-500 bg-transparent border border-red-500 rounded-full group">
+        <span className="absolute top-0 left-0 w-0 h-0 transition-all duration-200 border-t-2 border-red-300 group-hover:w-full ease"></span>
+        <span className="absolute bottom-0 right-0 w-0 h-0 transition-all duration-200 border-b-2 border-red-300 group-hover:w-full ease"></span>
+        <span className="absolute top-0 left-0 w-full h-0 transition-all duration-300 delay-200 bg-red-300 group-hover:h-full ease"></span>
+        <span className="absolute bottom-0 left-0 w-full h-0 transition-all duration-300 delay-200 bg-red-300 group-hover:h-full ease"></span>
+        <span className="absolute inset-0 w-full h-full duration-300 delay-200 bg-red-500 opacity-0 group-hover:opacity-100"></span>
+        <span className="font-montserrat relative transition-colors duration-300 delay-200 group-hover:text-white ease">Sign in</span>
+    </a>
+  )
+}
+
+export default SignInButton
---
+++ src/components/SignUpButton.jsx
@@ -0,0 +1,13 @@
+const SignUpButton = () => {
+  return (
+    <a href="#" className="relative inline-flex items-center px-12 py-3 overflow-hidden text-lg font-medium text-red-500 border border-red-500 rounded-full hover:text-white group hover:bg-gray-50">
+        <span className="absolute left-0 block w-full h-0 transition-all bg-red-500 opacity-100 group-hover:h-full top-1/2 group-hover:top-0 duration-400 ease"></span>
+        <span className="absolute right-0 flex items-center justify-start w-10 h-10 duration-300 transform translate-x-full group-hover:translate-x-0 ease">
+        <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M14 5l7 7m0 0l-7 7m7-7H3"></path></svg>
+        </span>
+        <span className="relative">Sign Up</span>
+    </a>
+  )
+}
+
+export default SignUpButton
