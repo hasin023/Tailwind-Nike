874f89ad0a1a0f18622fff09fd2b5fa640a8f841
Hasin Mahtab <hasinmahtab.alvee@gmail.com>
2023-08-20 10:56:29 +0600

    Finished popular products section

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -37,7 +37,7 @@ const Hero = () => {
       </div>
 
       {/* RIGHT SIDE */}
-      <div className="relative flex-1 flex justify-center items-center xl:min-h-screen max-xl:py-40 ">
+      <div className="relative flex-1 flex justify-center items-center xl:min-h-screen max-xl:py-40">
         <img src={bigShoeImg} alt="shoeCOllection" height={400} width={500} className="object-contain relative z-10"/>
 
         <div className="flex sm:gap-6 gap-4 absolute -bottom-[5%] sm:left-[10%] max-sm:px-6">
