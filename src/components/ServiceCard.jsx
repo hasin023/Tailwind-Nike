b6aa9d3012a2aa151fc30bbd4159ce118feee8fc
Hasin Mahtab <hasinmahtab.alvee@gmail.com>
2023-08-21 22:37:58 +0600

    Finished Customer review section

--- src/components/ServiceCard.jsx
+++ src/components/ServiceCard.jsx
@@ -1,6 +1,6 @@
 const ServiceCard = ({ imgURL, label, subtext }) => {
   return (
-    <div className='flex-1 sm:w-[350px] sm:min-w-[350px] w-full rounded-[20px] shadow-3xl px-10 py-16 bg-gradient-to-tr from-amber-300 to-rose-400'>
+    <div className='flex-1 sm:w-[350px] sm:min-w-[350px] w-full rounded-[20px] shadow-3xl px-10 py-16 bg-gradient-to-tr from-amber-500 to-rose-400'>
       <div className='w-11 h-11 flex justify-center items-center'>
         <img src={imgURL} alt={label} width={37} height={37} />
       </div>
