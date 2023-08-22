08039fdf0a5ca93bbe87b3edd7c05e3386b4dadf
Hasin Mahtab <hasinmahtab.alvee@gmail.com>
2023-08-22 13:47:54 +0600

    Added subscribe section

--- src/sections/Subscribe.jsx
+++ src/sections/Subscribe.jsx
@@ -1,7 +1,23 @@
+import Button from "../components/Button";
+
 const Subscribe = () => {
   return (
-    <div>Subscribe</div>
-  )
-}
+    <section
+      id='contact-us'
+      className='max-container flex justify-between items-center max-lg:flex-col gap-10'
+    >
+      <h3 className='text-4xl leading-[68px] lg:max-w-md font-palanquin font-bold'>
+        Sign Up for{' '}
+        <span className='bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-coral-red inline-block'>Updates</span> & Newsletter
+      </h3>
+      <div className='lg:max-w-[40%] w-full flex items-center max-sm:flex-col gap-5 p-2.5 sm:border sm:border-slate-gray rounded-full'>
+        <input type='text' placeholder='example@email.com' className='input' />
+        <div className='flex max-sm:justify-end items-center max-sm:w-full'>
+          <Button label='Sign Up' textColor='text-white'/>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default Subscribe
+export default Subscribe;
